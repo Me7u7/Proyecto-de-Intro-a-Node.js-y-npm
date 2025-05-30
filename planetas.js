d8f9c9960fb8044a2e1cb4d80905e5c609367c47
Akeri <samaescii@gmail.com>
2025-05-29 22:06:38 -0600

    Add files via upload

---
+++ planetas.js
@@ -0,0 +1,30 @@
+const planetas = [
+{
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+},
+{
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+},
+{
+    nombre: "Marte",
+    descripcion: "Tiene el monte más alto del sistema solar: el Olympus Mons.",
+    descubiertoEn: "Desde la antigüedad."
+},
+{
+    nombre: "Saturno",
+    descripcion: "Su luna Titán tiene lagos de metano líquido.",
+    descubiertoEn: "Desde siempre."
+},
+{
+    nombre: "Júpiter",
+    descripcion: "Puede tener más de 79 lunas y una tormenta que dura siglos.",
+    descubiertoEn: "Desde la antigüedad."
+}
+];
+
+module.exports = planetas;
+
